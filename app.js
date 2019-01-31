e55640dbc6b72d389f8a7b4db8b4239c8379d78f
makeitwayne17 <bryantliu17@gmail.com>
2019-01-31 10:32:29 -0800

    done

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-var hello = require('./routes/hello');
+//var hello = require('./routes/hello');
+var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
-
 var app = express();
 
 // all environments
@@ -37,7 +37,10 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/hello/:userName', hello.view);
+//app.get('/hello/:userName', hello.view);
+//app.get('/project', project.viewProject);
+app.get('/project/:name', project.viewProject);
+// Example route
 // Example route
 // app.get('/users', user.list);
 
